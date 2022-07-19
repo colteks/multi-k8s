14d6cb3100281cb67a264c28da56ca208bfa9ee7
colteks <enotloc@gmail.com>
2022-07-18 17:57:02 -0700

    some changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t colteks01/multi-client:latest -t colteks01/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t colteks01/multi-server:latest -t colteks01/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t colteks01/multi-worker:latest -t colteks01/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push colteks01/multi-client:latest
 docker push colteks01/multi-server:latest
 docker push colteks01/multi-worker:latest
